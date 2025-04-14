ed9f7dfdca1c35f7b193ed5d9a09bea636b343f3
kimght <bamboo-hatted-kim@proton.me>
2025-04-14 22:08:32 +0300

    feat: add flags & fix order

--- src/stores/localizations.ts
+++ src/stores/localizations.ts
@@ -4,12 +4,16 @@ import { invoke } from "@tauri-apps/api/core";
 import { listen } from "@tauri-apps/api/event";
 
 const getFlag = async (localization: Localization) => {
+  const name = localization.flag.replace(/\.[^.]+$/, "");
+  const parts = localization.flag.split(".");
+  const extension = parts.length > 1 ? parts.pop() : "svg";
+
   try {
-    const flag = await import(`../assets/flags/${localization.flag}.svg`);
+    const flag = await import(`../assets/flags/${name}.${extension}`);
     return { flag: flag.default, id: localization.id };
   } catch {
     return {
-      flag: `https://purecatamphetamine.github.io/country-flag-icons/3x2/${localization.flag}.svg`,
+      flag: `https://purecatamphetamine.github.io/country-flag-icons/3x2/${name}.svg`,
       id: localization.id,
     };
   }
@@ -80,8 +84,14 @@ export class LocalizationsStore {
   }
 
   public get all() {
-    return Object.values(this.byId).sort((a, b) =>
-      a.name.localeCompare(b.name)
-    );
+    return Object.values(this.byId).sort((a, b) => {
+      let comparison = a.flag.localeCompare(b.flag);
+      
+      if (comparison === 0) {
+        comparison = a.name.localeCompare(b.name);
+      }
+
+      return comparison;
+    });
   }
 }
